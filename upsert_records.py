ceccc0390d7e8d4dc76073f36432ce89c6a373e6
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-06-19 10:45:31 +0530

    Updated requirements & upsert leads

--- upsert_records.py
+++ upsert_records.py
@@ -1,7 +1,7 @@
 import requests, json, foo, refresh_access_token
 from refresh_access_token import refresh_token
 
-def upsert():
+def upsert(lead):
 
     url = 'https://www.zohoapis.in/crm/v2/Leads/upsert'
 
@@ -12,27 +12,11 @@ def upsert():
     request_body = dict()
     record_list = list()
 
-    record_object_1 = {
-        'Last_Name': 'Changed-Name-1',
-        'Email': 'newcrmapi@zoho.com',
-        'Company': 'Zoho',
-        'Lead_Status': 'Contacted',
-    }
-
-    record_list.append(record_object_1)
-
-    record_object_2 = {
-        'Last_Name': 'New Lead-3',
-        'Email': 'newlead@zoho.com',
-        'Lead_Status': 'Attempted to Contact',
-        'Phone': '9887766540',
-    }
-
-    record_list.append(record_object_2)
+    record_list.append(lead)
 
     request_body['data'] = record_list
 
-    duplicate_check_fields = ['Email']
+    duplicate_check_fields = ['Mobile']
 
     request_body['duplicate_check_fields'] = duplicate_check_fields
 
@@ -47,9 +31,11 @@ def upsert():
     response = requests.post(url=url, headers=headers, data=json.dumps(request_body).encode('utf-8'))
 
     if response is not None:
-        print("HTTP Status Code : " + str(response.status_code))
-
-        print(response.json())
+        return response.json()
 
 if __name__ == "__main__":
-    upsert()
+    lead = {
+        "Last_Name": "Foobar",
+        "Mobile": "919999999999"
+    }
+    upsert(lead=lead)
